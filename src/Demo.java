8d063a5fa96d6d126d244407a6ff98bae9438bbb
Keezill <mamedov.murik.lezgin@gmail.com>
2021-06-07 22:29:27 +0300

    First task of hw12

---
+++ src/Demo.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Demo {
+    public static String read(String path) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentString;
+
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append(" \n ");
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            System.out.println("File not found! Change path and try again! " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(read("C:\\.test.txt"));
+    }
+}
